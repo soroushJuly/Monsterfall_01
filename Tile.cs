2a7f5e5f860085c4971749f720c8128661567e83
soroushJuly <soroushjuly@gmail.com>
2024-02-10 13:59:54 +0000

    add: tile class

---
+++ Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Monsterfall_01
+{
+    internal class Tile
+    {
+        public Texture2D tileTexture;
+        private Vector2 position;
+
+        public Tile Initialize(Texture2D texture, Vector2 position)
+        {
+            this.tileTexture = texture;
+            this.position = position;
+
+            return this;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tileTexture, position, Color.White);
+        }
+    }
+}
