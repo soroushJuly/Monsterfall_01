1e3b1dc43dde87673f035ac79072464e83e31d1c
soroushJuly <soroushjuly@gmail.com>
2024-02-19 18:54:54 +0000

    add: collision to tile

--- Tile.cs
+++ Tile.cs
@@ -4,16 +4,20 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Monsterfall_01
 {
-    internal class Tile
+    internal class Tile : Collidable
     {
         public Texture2D tileTexture;
         private Vector2 position;
 
-        public Tile() { }
-        public Tile(Texture2D tileTexture, Vector2 position)
+        public Tile() { this.isCollidable = false; }
+        public Tile(Texture2D tileTexture, Vector2 position, bool isCollidable)
         {
             this.tileTexture = tileTexture;
             this.position = position;
+            if (!isCollidable) { 
+                this.isCollidable = false; 
+            }
+            this.box = new Rectangle((int)(position.X + tileTexture.Width / 4), (int)(position.Y), tileTexture.Width / 2, tileTexture.Height);
         }
 
         public Tile Initialize(Texture2D texture, Vector2 position)
@@ -23,8 +27,14 @@ namespace Monsterfall_01
 
             return this;
         }
-        public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch, GraphicsDevice GraphicsDevices)
         {
+            if (isCollidable)
+            {
+                Texture2D pixel = new Texture2D(GraphicsDevices, 1, 1);
+                pixel.SetData<Color>(new Color[] { Color.Red });
+                spriteBatch.Draw(pixel, this.box, Color.White);
+            }
             spriteBatch.Draw(tileTexture, position, Color.White);
         }
     }
