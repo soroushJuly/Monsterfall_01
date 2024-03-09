a280391732149356700c1775d62e2b938bbac533
soroushJuly <soroushjuly@gmail.com>
2024-03-09 15:24:44 +0000

    add: controls list

--- StatesMenu/StateMenuControls.cs
+++ StatesMenu/StateMenuControls.cs
@@ -10,6 +10,8 @@ namespace Monsterfall_01.StatesMenu
     internal class StateMenuControls : State
     {
         private ButtonList ButtonList;
+        private TextList ControlsList;
+        private TextList ActionsList;
         public event EventHandler Back;
         private int offsetX;
         private int offsetY;
@@ -26,7 +28,11 @@ namespace Monsterfall_01.StatesMenu
         public override void Enter(object owner)
         {
             ButtonList = new ButtonList(buttonIndicator, offsetX, offsetY, font, 50);
+            ControlsList = new TextList(offsetX, offsetY + 30, font, Color.DarkOliveGreen, 25);
+            ActionsList = new TextList(offsetX + 50, offsetY + 30, font, Color.DarkOliveGreen, 25);
             LoadMainButtons();
+            LoadControlsText();
+            LoadActionsText();
         }
         public override void Exit(object owner)
         {
@@ -41,9 +47,29 @@ namespace Monsterfall_01.StatesMenu
             ButtonList.AddButton("Back");
             ButtonList.ButtonClicked += this.HandleButtonSelection;
         }
+        private void LoadControlsText()
+        {
+            ControlsList.AddText("W:");
+            ControlsList.AddText("S:");
+            ControlsList.AddText("A:");
+            ControlsList.AddText("D:");
+            ControlsList.AddText("J:");
+            ControlsList.AddText("E:");
+        }
+        private void LoadActionsText()
+        {
+            ActionsList.AddText("Move North");
+            ActionsList.AddText("Move South");
+            ActionsList.AddText("Move West");
+            ActionsList.AddText("Move East");
+            ActionsList.AddText("Shoot Arrow");
+            ActionsList.AddText("Interact/Buy");
+        }
         public override void Draw(object owner, GameTime gameTime, SpriteBatch spriteBatch = null)
         {
             ButtonList.Draw(spriteBatch);
+            ControlsList.Draw(spriteBatch);
+            ActionsList.Draw(spriteBatch);
         }
         private void HandleButtonSelection(object sender, Button button)
         {
