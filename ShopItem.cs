c0e681269d28a136db16b1e9f6672ce40f8112f3
soroushJuly <soroushjuly@gmail.com>
2024-03-09 20:23:42 +0000

    add: text to the shopItem

--- ShopItem.cs
+++ ShopItem.cs
@@ -9,10 +9,11 @@ namespace Monsterfall_01
     {
         Texture2D texture;
         Vector2 position;
-        const float SCALE = 0.6f;
+        const float SCALE = 0.7f;
 
         protected int cost;
         bool isInRange;
+        public int GetCost() { return cost; }
         public ShopItem(Texture2D texture) 
         {
             this.texture = texture;
@@ -23,15 +24,16 @@ namespace Monsterfall_01
         public void Initialize(Vector2 position)
         {
             this.position = position;
-            this.box = new Rectangle((int)position.X, (int)position.Y, (int)(texture.Width * SCALE), (int)(texture.Height * SCALE));
+            this.box = new Rectangle((int)position.X, (int)position.Y, (int)(texture.Width * SCALE / 1.5f), (int)(texture.Height * SCALE));
         }
 
-        public override void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
+        public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, SpriteFont font)
         {
             //DrawBoundingBox(spriteBatch, graphicsDevice);
             Color color = Color.Gray;
             if (isInRange) { color = Color.White; }
             spriteBatch.Draw(texture, new Rectangle((int)position.X, (int)position.Y, (int)(texture.Width * SCALE), (int)(texture.Height * SCALE)), color);
+            spriteBatch.DrawString(font, cost.ToString(), new Vector2((int)position.X + (int)(texture.Width * SCALE / 2) - 15, (int)position.Y + (int)(texture.Height * SCALE) - 15), color);
         }
 
         public override bool CollisionTest(Collidable obj)
