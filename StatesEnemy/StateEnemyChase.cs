9d5eb01830f8a27ea417d82697113d1d9ec8a82b
soroushJuly <soroushjuly@gmail.com>
2024-03-12 18:13:23 +0000

    add: speed movement to enemy chase

--- StatesEnemy/StateEnemyChase.cs
+++ StatesEnemy/StateEnemyChase.cs
@@ -15,15 +15,21 @@ namespace Monsterfall_01.StatesEnemy
         {
             Enemy enemy = owner as Enemy;
             if (enemy == null) { return; }
+            // Change animation to run
             enemy.currentState = Enemy.States.RUN;
+            // increase movement speed on enter
+            enemy.enemyMoveSpeed += 0.5f;
         }
         public override void Exit(object owner)
         {
+            Enemy enemy = owner as Enemy;
+            if (enemy == null) { return; }
+            // subtract the added value
+            enemy.enemyMoveSpeed -= 0.5f;
         }
         public override void Execute(object owner, GameTime gameTime)
         {
             Enemy enemy = owner as Enemy;
-
             if (enemy == null) { return; }
         }
     }
